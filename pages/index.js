2f5b05ca98f756aaddaa6562b5457b9fe71abb3c
AnniLevi <al.devpy@gmail.com>
2023-09-27 17:08:54 +0200

    add server side code in getStaticProps for rendering a list of meetups

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,28 @@
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "https://images.pexels.com/photos/1181406/pexels-photo-1181406.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    address: "Some address 5, 12345 Some City",
-    description: "This is a first meetup",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image:
-      "https://images.pexels.com/photos/705792/pexels-photo-705792.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    address: "Some address 6, 12345 Some City",
-    description: "This is a second meetup",
-  },
-  {
-    id: "m3",
-    title: "A Third Meetup",
-    image:
-      "https://cafesydney.com/cms_uploads/images/18_cafe_sydney_310315_001338.jpg",
-    address: "Some address 7, 12345 Some City",
-    description: "This is another",
-  },
-  {
-    id: "m4",
-    title: "A Fourth Meetup",
-    image: "https://www.digital.brussels/wp-content/uploads/2018/06/Meetup.jpg",
-    address: "Some address 8, 12345 Some City",
-    description: "The last one",
-  },
-];
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
 export async function getStaticProps() {
-  //fetch data from an API
+  //this code will never be exposed to the client
+  const client = await MongoClient.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@atlascluster.rz5wcke.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        id: meetup._id.toString(),
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+      })),
     },
     revalidate: 10,
   };
